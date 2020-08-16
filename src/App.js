66f3d5ec06928912511e69d000a65505c309cfc0
Berman Benjamin <bermanschultz29@gmail.com>
2020-08-16 18:59:29 -0300

    create react app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>My first react app</h1>
       </header>
     </div>
   );
